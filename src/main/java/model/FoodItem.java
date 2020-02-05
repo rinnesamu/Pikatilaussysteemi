531e1ccf0cb339c92a09a705ce786e23aa17d9cf
Samu <samu.rinne@metropolia.fi>
2020-02-05 16:21:35 +0200

    Added category, sold amount and ready amount and tests for food items

--- src/main/java/model/FoodItem.java
+++ src/main/java/model/FoodItem.java
@@ -20,6 +20,12 @@ public class FoodItem {
 	private double price;
 	@Column
 	private boolean inMenu;
+	@Column
+	private String category;
+	@Column
+	private int sold;
+	@Column
+	private int ready;
 	
 	public FoodItem() {
 		
@@ -29,6 +35,9 @@ public class FoodItem {
 		this.price = price;
 		this.name = name;
 		this.inMenu = inMenu;
+		this.category = "None";
+		this.sold = 0;
+		this.ready = 0;
 	}
 
 	public double getPrice() {
@@ -62,9 +71,59 @@ public class FoodItem {
 	public void setInMenu(boolean inMenu) {
 		this.inMenu = inMenu;
 	}
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getSold() {
+		return sold;
+	}
+
+	public void setSold(int sold) {
+		if (sold < 0) {
+			this.sold = 0;
+		} else {
+			this.sold = sold;
+		}
+	}
+
+	public int getReady() {
+		return ready;
+	}
+
+	public void setReady(int ready) {
+		if (ready < 0) {
+			this.ready = 0;
+		}else {
+			this.ready = ready;
+		}
+	}
+	
+	public void increaseSold() {
+		this.sold++;
+		
+	}
+	
+	public void increaseReady() {
+		this.ready++;
+		
+	}
+	
+	public void decreaseReady() {
+		if (this.ready > 0) {
+			this.ready--;
+		}
+		
+	}
 	
 	public String toString() {
 		return this.name + ", " + this.price + ", " + this.inMenu;
 	}
+
+	
 
 }
--- src/test/java/FoodItemTest.java
+++ src/test/java/FoodItemTest.java
@@ -57,10 +57,76 @@ class FoodItemTest {
 		assertEquals(false, foodItem.isInMenu(), "Couldn't take away from menu");
 	}
 	
+	@Test
 	@DisplayName("Change menu asctivity: true -> false")
 	void testSetInMenuFalse() {
 		foodItem.setInMenu(true);
 		assertEquals(true, foodItem.isInMenu(), "Couldn't take away from menu");
 	}
+	
+	@Test
+	@DisplayName("Test getting ready ammount")
+	void testGetReady() {
+		assertEquals(0, foodItem.getReady(), "Couldn't get correct ready ammount.");
+	}
+	
+	@Test
+	@DisplayName("Test setting ready ammount")
+	void testSetReady() {
+		foodItem.setReady(5);
+		assertEquals(5, foodItem.getReady(), "Couldn't set correct ready ammount.");
+		foodItem.setReady(-5);
+		assertEquals(0, foodItem.getReady(), "Negative number should result 0");
+	}
+	
+	@Test
+	@DisplayName("Test getting category")
+	void testGetCategory() {
+		assertEquals("None", foodItem.getCategory(), "Couldn't get correct category.");
+	}
+	
+	@Test
+	@DisplayName("Test setting category")
+	void testSetCategory() {
+		foodItem.setCategory("Burger");
+		assertEquals("Burger", foodItem.getCategory(), "Couldn't set correct category.");
+	}
+	
+	@Test
+	@DisplayName("Test getting sold ammount")
+	void testGetSold() {
+		assertEquals(0, foodItem.getSold(), "Couldn't get correct sold ammount.");
+	}
+	
+	@Test
+	@DisplayName("Test setting sold ammount")
+	void testSetSold() {
+		foodItem.setSold(5);
+		assertEquals(5, foodItem.getSold(), "Couldn't set correct sold ammount.");
+		foodItem.setSold(-5);
+		assertEquals(0, foodItem.getSold(), "Negative number should result 0");
+	}
+	
+	@Test
+	@DisplayName("Test increase sold ammount")
+	void testIncreaseSold() {
+		foodItem.increaseSold();
+		assertEquals(1, foodItem.getSold(), "Couldn't get correct sold ammount.");
+		foodItem.increaseSold();
+		foodItem.increaseSold();
+		assertEquals(3, foodItem.getSold(), "Couldn't get correct sold ammount.");
+	}
+	
+	@Test
+	@DisplayName("Test increase and decrease ready ammount")
+	void testIncreaseReady() {
+		foodItem.increaseReady();
+		assertEquals(1, foodItem.getReady(), "Couldn't get correct ready ammount.");
+		foodItem.decreaseReady();
+		assertEquals(0, foodItem.getReady(), "Couldn't get correct ready ammount.");
+		foodItem.decreaseReady();
+		assertEquals(0, foodItem.getReady(), "Couldn't get correct ready ammount.");
+	}
+
 
 }
