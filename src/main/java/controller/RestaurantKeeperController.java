1b67f7b13e2f5eefdd41d68cef9bb9dca8f533c6
aseuna <arttuseuna@gmail.com>
2020-02-06 14:18:59 +0200

    restaurantKeeper ui init

---
+++ src/main/java/controller/RestaurantKeeperController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import application.RestaurantKeeperGUI;
+import javafx.fxml.FXML;
+
+import model.*;
+
+/**
+ * Kontrolleri luokka ravintoloitsijan käyttölittymälle.
+ * Määritellään käyttäliittymän nappien toiminnot.
+ * 
+ * @author Arttu Seuna
+ *
+ */
+public class RestaurantKeeperController {
+	
+	private FoodItem foodItem;
+	private FoodItemAccessObject foodItemDao;
+	
+	/**
+	 * Empty constructor
+	 */
+	public RestaurantKeeperController() {
+		
+	}
+	
+
+	@FXML
+	public void initialize() {
+		
+	}
+	
+
+}
