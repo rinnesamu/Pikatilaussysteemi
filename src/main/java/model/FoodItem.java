93cc0aee9c927663674aac873c9423b8c6c2cc8b
Samu <samu.rinne@metropolia.fi>
2020-02-09 20:22:41 +0200

    Add jdoc for FoodItem

--- src/main/java/model/FoodItem.java
+++ src/main/java/model/FoodItem.java
@@ -2,7 +2,7 @@ package model;
 import javax.persistence.*;
 
 /**
- * 
+ * Represents all food items
  * @author Samu Rinne
  *
  */
@@ -27,10 +27,20 @@ public class FoodItem {
 	@Column
 	private int ready;
 	
+	/**
+	 * Empty constructor for hibernate
+	 */
 	public FoodItem() {
 		
 	}
 	
+	/**
+	 * 
+	 * Constructor
+	 * @param name Items name
+	 * @param price Items price
+	 * @param inMenu Is item in active menu
+	 */
 	public FoodItem(String name, double price, boolean inMenu) {
 		this.price = price;
 		this.name = name;
@@ -40,6 +50,13 @@ public class FoodItem {
 		this.ready = 0;
 	}
 	
+	/**
+	 * Constructor in case you need to add category also
+	 * @param name Items name
+	 * @param price Items price
+	 * @param category Items category
+	 * @param inMenu Is item in active menu
+	 */
 	public FoodItem(String name, double price, String category, boolean inMenu) {
 		this.price = price;
 		this.name = name;
@@ -49,49 +66,90 @@ public class FoodItem {
 		this.ready = 0;
 	}
 
+	/**
+	 * Getter of price
+	 * @return price
+	 */
 	public double getPrice() {
 		return price;
 	}
 
+	/**
+	 * Setter for price
+	 * @param price items price
+	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
 
+	/**
+	 * Getter for name
+	 * @return name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Setter for name
+	 * @param name items name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Getter for id
+	 * @return itemId
+	 */
 	public int getItemId() {
 		return ItemId;
 	}
 
-	public void setItemId(int itemId) {
-		ItemId = itemId;
-	}
-
+	/**
+	 * Getter for inMenu
+	 * @return inMenmu
+	 */
 	public boolean isInMenu() {
 		return inMenu;
 	}
 
+	/**
+	 * Setter for inMenu
+	 * @param inMenu
+	 */
 	public void setInMenu(boolean inMenu) {
 		this.inMenu = inMenu;
 	}
+	
+	/**
+	 * Getter for category
+	 * @return category
+	 */
 	public String getCategory() {
 		return category;
 	}
 
+	/**
+	 * Setter for category
+	 * @param category items category
+	 */
 	public void setCategory(String category) {
 		this.category = category;
 	}
 
+	/**
+	 * Getter for sold amount
+	 * @return sold
+	 */
 	public int getSold() {
 		return sold;
 	}
 
+	/**
+	 * Setter for sold amount
+	 * @param sold how many items have been sold
+	 */
 	public void setSold(int sold) {
 		if (sold < 0) {
 			this.sold = 0;
@@ -100,10 +158,18 @@ public class FoodItem {
 		}
 	}
 
+	/**
+	 * Getter for ready products
+	 * @return ready
+	 */
 	public int getReady() {
 		return ready;
 	}
 
+	/**
+	 * Setter for ready amount
+	 * @param ready how many items are ready
+	 */
 	public void setReady(int ready) {
 		if (ready < 0) {
 			this.ready = 0;
@@ -112,16 +178,25 @@ public class FoodItem {
 		}
 	}
 	
+	/**
+	 * Increases sold amount by one
+	 */
 	public void increaseSold() {
 		this.sold++;
 		
 	}
 	
+	/**
+	 * Increases ready amount by one
+	 */
 	public void increaseReady() {
 		this.ready++;
 		
 	}
 	
+	/**
+	 * Decreases reaady amount by one
+	 */
 	public void decreaseReady() {
 		if (this.ready > 0) {
 			this.ready--;
