6cd8444916bc45d91b81fb4f9ccb91a08a0167b2
kimmoperala <kimmo.perala@metropolia.fi>
2020-02-16 16:09:27 +0200

    Better structure and amount numbers of shopping list working

--- src/main/java/view/MenuViewController.java
+++ src/main/java/view/MenuViewController.java
@@ -1,9 +1,12 @@
 package view;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -60,10 +63,7 @@ public class MenuViewController {
 	private ShoppingCart shoppingCart = new ShoppingCart();
 	
 	private FoodItem[] items;
-	
-	private FoodItem[] allItems = foodItemAO.readFoodItems();
-
-	
+		
 	public MenuViewController() {
 		
 	}
@@ -120,7 +120,7 @@ public class MenuViewController {
 		for (int i = 0; i < items.length; i++) {
 			if (items[i].isInMenu()) {
 				Button menuItem = new Button();
-				
+
 				// FoodItem from the category
 				FoodItem fItem = items[i];
 				//System.out.println("fItem on " + fItem);
@@ -136,93 +136,91 @@ public class MenuViewController {
 				iv.setFitHeight(60);
 				iv.setFitWidth(60);
 				menuItem.setGraphic(iv);
-				//menuItem.setText(Integer.toString(menuId));
-				menuItem.setText(fItem.getName());
+				menuItem.setText(Integer.toString(menuId));
+				//menuItem.setText(fItem.getName());
 				menuItem.setContentDisplay(ContentDisplay.BOTTOM);
 				menu.getChildren().add(menuItem);
-				
-				EventHandler<ActionEvent> menuButtonHandler = new EventHandler<ActionEvent>() {
-					
-					@Override
-					public void handle (ActionEvent event)
-					{
-						// Getting the id back
-						Button sCartItem = new Button("");
-						int id = Integer.parseInt(menuItem.getId());
-						sCartItem.setId(Integer.toString(id));
-						
-						// Get all the item numbers in shopping cart
-						int[] listOfItemIds= shoppingCart.getAllItemId();
-						System.out.println("listOfItemIds " + Arrays.toString(listOfItemIds));
-						boolean found = false;
-						for (int i = 0; i < listOfItemIds.length; i++) {
-							if (id == listOfItemIds[i]) {
-								found = true;
-							}	
-						}
-						
-						// If item is already there, increase the amount
-						if (found) {
-							int oldAmount = shoppingCart.getAmount(id);
-							shoppingCart.setAmount(fItem, (oldAmount+1));
-							sCartItem.setText(fItem.getName() + " " + shoppingCart.getAmount(id));
-						}
-						// Otherwise add to the shopping cart
-						else {
-							shoppingCart.addToShoppingCart(fItem, 1);
-							System.out.println(shoppingCart);
-							sCartItem.setMinSize(170, 50);
-							sCartItem.setText(fItem.getName() + " " + shoppingCart.getAmount(id));
-							shoppingCartList.getChildren().add(sCartItem);
-						}
-						System.out.println(shoppingCart);
-						// Pressing the items on the shoppingCartList
-						EventHandler<ActionEvent> sCartButtonHandler = new EventHandler<ActionEvent>() {
-							
-							@Override
-							public void handle (ActionEvent event)
-							{
-								Alert options = new Alert(AlertType.CONFIRMATION);
-								options.setTitle("Valitse " + fItem.getName() + " määrä");
-								int amountNow = shoppingCart.getAmount(id);
-								options.setHeaderText("Määrä:  " + amountNow);
-								options.setContentText("Valitse lisäys tai vähennys");
-
-								ButtonType increase = new ButtonType("Lisää");
-								ButtonType decrease = new ButtonType("Vähennä");
-								ButtonType okay = new ButtonType("OK",ButtonData.OK_DONE);
-								ButtonType cancel = new ButtonType("Peruuta", ButtonData.CANCEL_CLOSE);
-								
-								options.getButtonTypes().setAll(increase, decrease, okay, cancel);
-								Optional<ButtonType> result = options.showAndWait();
-								
-								if (result.get() == increase) {
-									amountNow += 1;
-									shoppingCart.setAmount(fItem, amountNow);
-								}
-								if (result.get() == decrease) {
-									amountNow -= 1;
-									shoppingCart.setAmount(fItem, amountNow);
-								}
-								if (result.get() == okay) {
-									
-								}
-								if (result.get() == cancel){
-									
-								}
-							}
-						};
-						sCartItem.setOnAction (sCartButtonHandler);
-						if (shoppingCart.getAmount(id) == 0) {
-							// then is removed from shoppingCartList
-						};
-					}
-				};
-				// Setting a mouse event
-				menuItem.setOnAction(menuButtonHandler);
-				
+				menuItem.setOnAction(event -> menuButtonHandler(fItem, menuItem));
 			}
+		}
+	}
+	
+	private void menuButtonHandler(FoodItem foodItem, Button button) {
+		
+			Button sCartItem = new Button("");
+			int id = Integer.parseInt(button.getId());
+			sCartItem.setId(Integer.toString(id));
 			
+			// Get all the item numbers in shopping cart
+			int[] listOfItemIds= shoppingCart.getAllItemId();
+			System.out.println("listOfItemIds " + Arrays.toString(listOfItemIds));
+			boolean found = false;
+			for (int i = 0; i < listOfItemIds.length; i++) {
+				if (id == listOfItemIds[i]) {
+					found = true;
+				}	
+			}
+			// If item is already there, increase the amount
+			if (found) {
+				int oldAmount = shoppingCart.getAmount(id);
+				shoppingCart.setAmount(foodItem, (oldAmount+1));
+				shoppingCartList.getChildren().remove(sCartItem);
+				System.out.println("scl on " + shoppingCartList.getChildren());
+				// korvaa id -> fItem.getName()
+				System.out.println("getID ON " + sCartItem.getId());
+				for (int i = 0; i < shoppingCartList.getChildren().size(); i++) {
+					if (id == Integer.parseInt(shoppingCartList.getChildren().get(i).getId())) {
+						shoppingCartList.getChildren().remove(i);
+					}
+				}				
+				sCartItem.setText(id + " " + shoppingCart.getAmount(id));
+				shoppingCartList.getChildren().add(sCartItem);
+			}
+			// Otherwise add to the shopping cart
+			else {
+				shoppingCart.addToShoppingCart(foodItem, 1);
+				System.out.println(shoppingCart);
+				sCartItem.setMinSize(170, 50);
+				sCartItem.setText(id + " " + shoppingCart.getAmount(id));
+				shoppingCartList.getChildren().add(sCartItem);
+			}
+			System.out.println(shoppingCart);
+			sCartItem.setOnAction(event -> sCartButtonHandler(foodItem, id));
+	}
+	
+	private void sCartButtonHandler (FoodItem foodItem, int id) {
+
+		Alert options = new Alert(AlertType.CONFIRMATION);
+		options.setTitle("Valitse " + foodItem.getName() + " määrä");
+		int amountNow = shoppingCart.getAmount(id);
+		options.setHeaderText("Määrä:  " + amountNow);
+		options.setContentText("Valitse lisäys tai vähennys");
+	
+		ButtonType increase = new ButtonType("Lisää");
+		ButtonType decrease = new ButtonType("Vähennä");
+		ButtonType okay = new ButtonType("OK",ButtonData.OK_DONE);
+		ButtonType cancel = new ButtonType("Peruuta", ButtonData.CANCEL_CLOSE);
+		
+		options.getButtonTypes().setAll(increase, decrease, okay, cancel);
+		Optional<ButtonType> result = options.showAndWait();
+		
+		if (result.get() == increase) {
+			amountNow += 1;
+			shoppingCart.setAmount(foodItem, amountNow);
+		}
+		if (result.get() == decrease) {
+			amountNow -= 1;
+			shoppingCart.setAmount(foodItem, amountNow);
+		}
+		if (result.get() == okay) {
+			
+		}
+		if (result.get() == cancel){
+			
+		}
+		
+		if (shoppingCart.getAmount(id) == 0) {
+			// then is removed from shoppingCartList
 		}
 	}
 	
