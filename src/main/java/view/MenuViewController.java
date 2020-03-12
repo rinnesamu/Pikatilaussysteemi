619311147457dd033770fc53fd43654c5d40a548
kimmoperala <kimmo.perala@metropolia.fi>
2020-03-12 18:23:34 +0200

    JavaDoc Comments for MVController fixed

--- src/main/java/view/MenuViewController.java
+++ src/main/java/view/MenuViewController.java
@@ -125,6 +125,7 @@ public class MenuViewController {
 	
 	/**
 	 * Method for creating the category list menu.
+	 * @param categories Categories for creating the categories list menu.
 	 */
 	private void createCategoryList(Category[] categories) {
 
@@ -356,7 +357,6 @@ public class MenuViewController {
 	 * Button handler for the menubuttons. Adds items to the shopping cart.
 	 * 
 	 * @param foodItem The fooditem tied to the particular button.
-	 * @param button The created menubutton.
 	 */
 	private void menuButtonHandler(FoodItem foodItem) {
 		Button sCartItem = new Button("");
@@ -404,9 +404,9 @@ public class MenuViewController {
 	}
 	
 	/**
-	 * Ingredients of an item are retrieved from the local foodItem object
+	 * Ingredients of an item are retrieved from the local foodItem object.
 	 * @param foodItem Fooditem of which ingredients are retrieved.
-	 * @return
+	 * @return Ingredients of the local foodItem object.
 	 */
 	private ArrayList<String> getObjectIngredients(FoodItem foodItem) {
 		//String[] ingredientsNames;
@@ -432,7 +432,7 @@ public class MenuViewController {
 	/**
 	 * 	Removable ingredients of an item are retrieved from the database ie. original ingredients.
 	 * @param foodItem Fooditem of which ingredients are retrieved.
-	 * @return
+	 * @return Ingredients of the database object.
 	 */
 	private ArrayList<String> getDatabaseIngredients(FoodItem foodItem) {
 		
