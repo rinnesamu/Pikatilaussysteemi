0bb1cfe97891cda2ba873855d2decf2ed0fc2224
Samu <samu.rinne@metropolia.fi>
2020-03-30 16:21:59 +0300

    add locale and first resource bundles

--- src/main/java/application/Start.java
+++ src/main/java/application/Start.java
@@ -1,6 +1,12 @@
 package application;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 import controller.TimingController;
 import javafx.application.Application;
@@ -18,11 +24,38 @@ public class Start extends Application {
 	private AnchorPane rootLayout;
 	FXMLLoader loader;
 	private TimingController control;
+	Locale curLocale = new Locale("fi", "FI"); // Default Finland
+	ResourceBundle bundle;
+	String appConfigPath = "Test.properties";
+	
+	@Override
+	public void init() {
+		Properties properties = new Properties();
+		try {
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream(appConfigPath);
+			properties.load(inputStream);
+			String language = properties.getProperty("languageEn");
+			String country = properties.getProperty("countryEn");
+			curLocale = new Locale(language, country);
+			Locale.setDefault(curLocale);
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Not found, using default");
+		}
+		try {
+			bundle = ResourceBundle.getBundle("TextResources", curLocale);
+			System.out.println("Ennen");
+		}catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Default TextProperties not found.");
+			System.exit(0);
+		}
+	}
 	
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Ravintolasovellus");
+		this.primaryStage.setTitle(bundle.getString("headerText"));
 		
 		control = new TimingController();
 		control.setControllable(this);
@@ -36,6 +69,7 @@ public class Start extends Application {
 		try {
 			loader = new FXMLLoader();
 			loader.setLocation(MainApp.class.getResource("/view/StartView.fxml"));
+			loader.setResources(bundle);
 			rootLayout = (AnchorPane) loader.load();
 			
 			Scene scene = new Scene(rootLayout);
