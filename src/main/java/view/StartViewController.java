0ec11497a286f9870c5c29a11559d7c01f476113
Samu <samu.rinne@metropolia.fi>
2020-05-06 16:23:33 +0300

    add javadoc for StartViewController

--- src/main/java/view/StartViewController.java
+++ src/main/java/view/StartViewController.java
@@ -11,21 +11,34 @@ public class StartViewController {
 	
 	private Start controller;
 	
+	/**
+	 * Starts order
+	 */
 	@FXML
 	public void startOrdering() {
 		controller.startOrder();
 	}
 	
+	/**
+	 * Changes language to Finnish
+	 */
 	@FXML 
 	public void setLanguageFi() {
 		 controller.setLanguage("languageFi", "countryFi");
 	 }
 	
+	/**
+	 * Changes language to Englis
+	 */
 	@FXML 
 	public void setLanguageEn() {
 		 controller.setLanguage("languageEn", "countryEn");
 	 }
 	 
+	/**
+	 * Setter for start
+	 * @param start
+	 */
 	 public void setController(Start start) {
 		 this.controller = start;
 	 }
