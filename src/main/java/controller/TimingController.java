b5973d843edd7bb80485ec1a569c05477a9c4ebb
Gadhan <mikaeltikka@hotmail.com>
2020-05-07 10:05:04 +0300

    Added documentation

--- src/main/java/controller/TimingController.java
+++ src/main/java/controller/TimingController.java
@@ -3,6 +3,13 @@ package controller;
 import application.Start;
 import javafx.application.Platform;
 
+/**
+ * A class that extends Thread.
+ * It starts a thread in the background and observes a Start class once it has been set via setControllable() method.
+ * 
+ * @author mikae
+ *
+ */
 public class TimingController extends Thread{
 	private static volatile Thread TimingController;
 	
@@ -14,11 +21,18 @@ public class TimingController extends Thread{
 	private long lastWake;
 	private Start control;
 	
+	/**
+	 * Starts the thread and observes the set Start class.
+	 */
 	public void start() {
         TimingController = new Thread(this);
         TimingController.start();
     }
 	
+	/**
+	 * The method that is running after the thread has been started via start().
+	 * Will continue running until cease() is called.
+	 */
 	public void run() {
 		Thread thisThread = Thread.currentThread();
 		while(TimingController == thisThread) {
@@ -52,20 +66,33 @@ public class TimingController extends Thread{
 		}
 	}
 	
+	/**
+	 * Stops the running, and thus the observing.
+	 */
 	public void cease() {
 		TimingController = null;
 	}
 	
+	/**
+	 * Sets the start class that this thread should observe.
+	 * @param start The Start class to be observed.
+	 */
 	public void setControllable(Start start) {
 		control = start;
 	}
 	
+	/**
+	 * Whenever an action is done, this method should be called to reset the timer.
+	 */
 	public void update() {
 		System.out.println("Click");
 		rest = notification = false;
 		lastWake = System.currentTimeMillis();
 	}
 	
+	/**
+	 * Sets the thread back to starting position.
+	 */
 	public void reset() {
 		rest = true;
 		notification = false;
